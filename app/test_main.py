3db6a4a6bb8578637bcc8c650062022f92708d07
Sergii Semenets <serhey.semenets@gmail.com>
2024-11-22 15:23:29 +0200

    sovution v2

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,38 @@
 import datetime
-from unittest.mock import patch, MagicMock
+from unittest import mock
 
 import pytest
 
 from app.main import outdated_products
 
 
-@pytest.fixture()
-def products() -> list:
-    return [
+@pytest.mark.parametrize(
+    "mocked_date,result",
+    [
+        (datetime.date(2022, 2, 2), ["duck"]),
+        (datetime.date(2022, 1, 30), []),
+        (datetime.date(2022, 2, 5), ["duck"]),
+        (datetime.date(2022, 2, 11), ["salmon", "chicken", "duck"]),
+    ],
+)
+def test_outdated_products(mocked_date: mock, result: list) -> None:
+    products = [
         {
             "name": "salmon",
-            "expiration_date": datetime.date(2024, 2, 10),
+            "expiration_date": datetime.date(2022, 2, 10),
             "price": 600
         },
         {
             "name": "chicken",
-            "expiration_date": datetime.date(2024, 2, 5),
+            "expiration_date": datetime.date(2022, 2, 5),
             "price": 120
         },
         {
             "name": "duck",
-            "expiration_date": datetime.date(2025, 11, 1),
+            "expiration_date": datetime.date(2022, 2, 1),
             "price": 160
-        }
+        },
     ]
-
-
-@patch("app.main.datetime")
-def test_outdated_products(mock_date: MagicMock, products: list) -> None:
-    mock_date.date.today.return_value = datetime.date(2024, 11, 2)
-    assert outdated_products(products) == ["salmon", "chicken"]
+    with mock.patch("datetime.date") as mock_date:
+        mock_date.today.return_value = mocked_date
+        assert outdated_products(products) == result
